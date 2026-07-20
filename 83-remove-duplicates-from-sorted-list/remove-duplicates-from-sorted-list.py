3e30bba07940d520d0d3584e9c6423f392413b55
Akshaya Chilka <chilkaakshaya22@gmail.com>
2026-07-20 15:10:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (28.63%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -16,8 +16,7 @@ class Solution:
         c = head
         while c and c.next:
             if c.val==c.next.val:
-                c.next = c.next.next
-              
+                c.next = c.next.next            
             else:
                 c= c.next
         return head
