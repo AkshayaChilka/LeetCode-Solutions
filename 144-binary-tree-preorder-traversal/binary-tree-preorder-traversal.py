8a9f28a9c9a7c724982019de91f713a0b930e13e
Akshaya Chilka <chilkaakshaya22@gmail.com>
2026-08-03 11:35:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.39%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        l1=[]
+        """l1=[]
         def helper(root):
             if root==None:
                 return
@@ -15,5 +15,18 @@ class Solution:
             helper(root.left)
             helper(root.right)
         helper(root)
-        return l1
+        return l1"""
+        if root==None:
+            return []
+        st=[root]
+        ans=[]
+        curr=root
+        while(st!=[]):
+            node=st.pop()
+            ans.append(node.val)
+            if node.right!=None:
+                st.append(node.right)
+            if node.left!=None:
+                st.append(node.left)
+        return ans
         
