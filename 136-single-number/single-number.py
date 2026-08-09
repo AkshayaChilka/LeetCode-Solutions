f83adb4574a037293a9dea5fe9af2734aa414362
Akshaya Chilka <chilkaakshaya22@gmail.com>
2026-08-09 22:13:57 +0530

    Time: 7 ms (29.28%) | Memory: 21.6 MB (18.29%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,22 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
+        """ans = 0
 
         for num in nums:
             ans ^= num
 
-        return ans
+        return ans"""
+
+        seen=set()
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+        return seen.pop()
+
+        
+            
 
 
 
