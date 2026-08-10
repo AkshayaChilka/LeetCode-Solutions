a4afc9313db14dd9fad93a810a61f95e7e89abef
Akshaya Chilka <chilkaakshaya22@gmail.com>
2026-08-10 13:48:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.46%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,14 +1,72 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        emptyset = set()
-        while(n!=1 and n not in emptyset):
-            emptyset.add(n)
-            sum=0
-            while(n!=0):
-                dig= n%10
-                sum += dig**2
-                n = n//10
-            n = sum   
-        return n==1
+        """sets=set()
+        while(n!=1 and n not in sets):
+            sets.add(n)
+            ans=0
+            while (n!=0):
+                dig=n%10
+                ans=dig**2+ans
+                n=n//10
+            n=ans
+        return n==1"""
+
+        def get_next(num):
+            total = 0
+            while num > 0:
+                digit = num % 10
+                total += digit * digit
+                num //= 10
+            return total
+        
+        slow = n
+        fast = get_next(n)
+        
+        while fast != 1 and slow != fast:
+            slow = get_next(slow)             # move 1 step
+            fast = get_next(get_next(fast))   # move 2 steps
+        
+        return fast == 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+            
+
 
         
