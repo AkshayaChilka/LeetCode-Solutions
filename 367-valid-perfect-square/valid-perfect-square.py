764150d6812a9b207b4b22ad785447ab388e2af3
Akshaya Chilka <chilkaakshaya22@gmail.com>
2026-08-11 18:55:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (57.07%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num < 2:
+            return True  # 0 and 1 are perfect squares
+    
+        left, right = 2, num // 2
+        while left <= right:
+            mid = (left + right) // 2
+            guess = mid * mid
+            
+            if guess == num:
+                return True
+            elif guess < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return False
+
+
+        
