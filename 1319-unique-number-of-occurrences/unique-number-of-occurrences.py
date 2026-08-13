8f67dc180ef11204629ba01305442f206e99ce9f
Akshaya Chilka <chilkaakshaya22@gmail.com>
2026-08-13 21:10:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.90%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        """hashmap={}
+        for num in arr:
+            hashmap[num]=hashmap.get(num,0)+1
+        return len(hashmap.values())==len(set(hashmap.values()))"""
+
+        from collections import Counter
+        
+        # Step 1: Count occurrences
+        freq = Counter(arr)
+        
+        # Step 2: Check uniqueness
+        return len(freq.values()) == len(set(freq.values()))
+        
+
+        
+
+
+
