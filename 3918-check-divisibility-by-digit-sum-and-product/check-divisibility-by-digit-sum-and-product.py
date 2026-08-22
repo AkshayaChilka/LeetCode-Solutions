df7b7a85a2f6ea818a25f551fe4436564de28dcb
Akshaya Chilka <chilkaakshaya22@gmail.com>
2026-08-22 22:25:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (91.31%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.py
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.py
@@ -1,6 +1,6 @@
 class Solution:
     def checkDivisibility(self, n: int) -> bool:
-        original=n
+        """original=n
         sum=0
         pro=1
         while n!=0:
@@ -9,7 +9,18 @@ class Solution:
             pro*=dig
             n=n//10
         total=sum+pro
-        return original%total==0
+        return original%total==0"""
+
+        digit_sum = 0
+        digit_product = 1
+        
+        for ch in str(n):
+            d = int(ch)
+            digit_sum += d
+            digit_product *= d
+        
+        total = digit_sum + digit_product
+        return n % total == 0
 
         
 
